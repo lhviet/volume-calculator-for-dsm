d700e45e39907cb826c55ae0d952bc0640c9b7c4
Junyoung Clare Jang <jjc9310@gmail.com>
2018-04-24 13:59:08 -0400

    Add simple volume calc

--- src/main.py
+++ src/main.py
@@ -1,19 +1,34 @@
 '''
 Main entry point
 '''
+# pylint: disable=locally-disabled
+import numpy as np
 # pylint: disable=import-error
 from osgeo import gdal, ogr
 # pylint: enable=import-error
 
-import clip
+import clip as c
 
 def main():
     '''Entry point function for testing'''
     tiff_path = 'example/sample00.tif'
     shape_path = 'example/sample00.shp'
+
     tiff = gdal.Open(tiff_path)
+    tiff_gt = tiff.GetGeoTransform()
+    tiff_arr = tiff.ReadAsArray()
+
+    band = tiff.GetRasterBand(1)
+    nodata = band.GetNoDataValue()
+    x_gap = tiff_gt[1]
+    y_gap = tiff_gt[4]
+
     shape = ogr.Open(shape_path)
-    clip.clip(tiff, shape)
+
+    clipped = c.clip(tiff_gt, tiff_arr, shape)
+
+    min_z = np.amin(clipped)
+    print('volume: ', ((clipped - min_z)[clipped != nodata] * x_gap * y_gap).sum())
 
 if __name__ == '__main__':
     main()
